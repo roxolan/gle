36918f9b9fa2db6c4d7c78e5841c619737401ea8
Vic on Mac <roxolanus@gmail.com>
2014-10-28 01:17:46 +0200

    TDD: UserServiceTest first, and then UserServiceImpl was written

---
+++ src/main/java/com/roxolanus/gle/service/UserServiceImpl.java
@@ -0,0 +1,210 @@
+package com.roxolanus.gle.service;
+
+import com.roxolanus.gle.dao.AssignmentSubmissionDao;
+import java.util.List;
+import com.roxolanus.gle.domain.*;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import com.roxolanus.gle.vo.Result;
+import com.roxolanus.gle.vo.ResultFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Transactional
+@Service("userService")
+public class UserServiceImpl extends AbstractService implements UserService {
+
+    @Autowired
+    protected AssignmentSubmissionDao assignmentSubmissionDao;
+    
+    public UserServiceImpl() {
+        super();
+    }
+
+    @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
+    @Override
+    public Result<User> find(String username, String actionUsername) {
+
+        if(isValidUser(actionUsername)) {
+
+            User user = userDao.findByUsername(username);
+            return ResultFactory.getSuccessResult(user);
+
+        } else {
+            return ResultFactory.getFailResult(USER_INVALID);
+        }
+    }
+
+    @Transactional(readOnly = false, propagation = Propagation.REQUIRED)
+    @Override
+    public Result<User> store(
+        String username,
+        String firstName,
+        String lastName,
+        String email,
+        String password,
+        Character adminRole,
+        String actionUsername) {
+
+        User actionUser = userDao.find(actionUsername);
+        
+        if (!actionUser.isAdmin()) {
+
+            return ResultFactory.getFailResult(USER_NOT_ADMIN);
+
+        }
+
+        if (username == null || username.trim().isEmpty() || email == null || email.trim().isEmpty() ) {
+
+            return ResultFactory.getFailResult("Unable to store a user without a valid non empty username/email");
+
+        }
+
+        // password could be String of empty text: allowed but not good!
+        if(password == null || password.length() == 0){
+
+            return ResultFactory.getFailResult("Unable to store a user without a valid non empty password");
+
+        }
+
+        if(!adminRole.equals('Y') && !adminRole.equals('N')){
+
+            return ResultFactory.getFailResult("Unable to store the user: adminRole must be Y or N");
+
+        }
+
+        // we will store username/email values in lowercase
+        username = username.trim();
+        email = email.trim();
+
+        // test if the user is already in the system
+        User user = userDao.findByUsername(username);
+
+        // see if the user can be found by email
+        User testByEmailUser = userDao.findByEmail(email);
+        boolean doInsert = false;
+
+        if(user == null){
+
+            // user with this username is not yet in system
+            if(testByEmailUser == null){
+                // there is no user with this email address in the system
+                user = new User();
+                user.setUsername(username);
+                user.setEmail(email);
+                doInsert = true;
+
+            } else {
+
+                return ResultFactory.getFailResult("Unable to add new user: the email address is already in use");
+            }
+            
+        } else {
+
+            // the user has been found: need to do a few email tests
+            if(testByEmailUser == null){
+
+                // the email can be set as no other users have it assigned
+                user.setEmail(email);
+
+            } else if(! user.equals(testByEmailUser)){
+                
+                // the user being modified is different to the user that has this email address
+                return ResultFactory.getFailResult("The email address is already in use by username="
+                        + testByEmailUser.getUsername()
+                        + "and cannot be assigned to " + username);
+            }
+        }
+
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setPassword(password);
+        user.setAdminRole(adminRole);
+
+        if(doInsert) {
+
+            userDao.persist(user);
+
+        } else {
+
+            user = userDao.merge(user);
+
+        }
+
+        return ResultFactory.getSuccessResult(user);
+
+    }
+
+    @Transactional(readOnly = false, propagation = Propagation.REQUIRED)
+    @Override
+    public Result<User> remove(String username, String actionUsername){
+
+        User actionUser = userDao.find(actionUsername);
+        
+        if (!actionUser.isAdmin()) {
+
+            return ResultFactory.getFailResult(USER_NOT_ADMIN);
+
+        }
+
+        if(username == null){
+
+            return ResultFactory.getFailResult("Unable to remove null User");
+
+        } 
+
+        User user = userDao.findByUsername(username);
+
+        List<AssignmentSubmission> userSubmissions = assignmentSubmissionDao.findUserSubmissions(user);
+
+        if(user == null) {
+
+            return ResultFactory.getFailResult("Unable to load User for removal with username=" + username);
+
+        } else if(userSubmissions.size() > 0) {
+
+            // tasks have logs assigned: not allowed to delete
+            return ResultFactory.getFailResult("Unable to remove User with username=" + username + " as valid assignment submissions are pending");
+
+        } else {
+
+            userDao.remove(user);
+            String msg = "User " + username + " was deleted by " + actionUsername;
+            logger.info(msg);
+            return ResultFactory.getSuccessResultMsg(msg);
+
+        }
+
+    }
+
+    @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
+    @Override
+    public Result<List<User>> findAll(String actionUsername){
+
+        if(isValidUser(actionUsername)) {
+
+            return ResultFactory.getSuccessResult(userDao.findAll());
+
+        } else {
+            return ResultFactory.getFailResult(USER_INVALID);
+        }
+
+    }
+
+    @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
+    @Override
+    public Result<User> findByUsernamePassword(String username, String password){
+
+        User user = userDao.findByUsernamePassword(username, password);
+
+        if(user == null){
+
+            return ResultFactory.getFailResult("Unable to verify user/password combination!");
+
+        } else {
+
+            return ResultFactory.getSuccessResult(user);
+        }
+
+    }
+}
---
+++ src/test/java/com/roxolanus/gle/service/UserServiceTest.java
@@ -0,0 +1,139 @@
+package com.roxolanus.gle.service;
+
+import com.roxolanus.gle.dao.AssignmentSubmissionDao;
+import com.roxolanus.gle.dao.UserDao;
+import com.roxolanus.gle.domain.AssignmentSubmission;
+import com.roxolanus.gle.domain.User;
+import com.roxolanus.gle.vo.Result;
+import java.util.List;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UserServiceTest extends AbstractServiceForTesting {
+
+    @Autowired
+    protected UserService userService;
+    @Autowired
+    protected AssignmentSubmissionDao assignmentSubmissionDao;
+    @Autowired
+    protected UserDao userDao;
+    private final String TEST_USERNAME = "vkotusenko";
+    
+    @Test
+    public void testFind() throws Exception {
+        
+        logger.debug("\nSTARTED testFind()\n");
+        Result<List<User>> allItems = userService.findAll(TEST_USERNAME);
+        
+        assertTrue(allItems.getData().size() > 0);
+
+        // get the first item in the list
+        User u1 = allItems.getData().get(0);
+
+        String username = u1.getId();
+
+        Result<User> u2= userService.find(username, TEST_USERNAME);
+
+        assertTrue(u1.equals(u2.getData()));
+        logger.debug("\nFINISHED testFind()\n");
+    }
+
+    @Test
+    public void testAddNew() throws Exception {
+
+        String ADMIN_USERNAME = "osavruk";
+
+        logger.debug("\nSTARTED testAddNew()\n");
+
+        Result<User> ar = userService.store("nusername", "David", "Francis", "df@tttracker.com", "admpwd", 'N', ADMIN_USERNAME);
+
+        // should succeed
+        logger.debug(ar.getMsg());
+        assertTrue(ar.isSuccess());
+
+        ar = userService.store(this.TEST_USERNAME, "David", "Francis", "df@tttracker.com", "admpwd", 'Y', ADMIN_USERNAME);
+
+        logger.debug(ar.getMsg());
+        assertTrue("Cannot assign email that is currently assigned to other user", !ar.isSuccess());
+
+        ar = userService.store("user100", "David", "Francis", "user100@tttracker.com", "", 'Y', ADMIN_USERNAME);
+
+        logger.debug(ar.getMsg());
+        assertTrue("Cannot set empty password for user", !ar.isSuccess());
+
+        ar = userService.store("user101", "David", "Francis", "  ", "validpwd", 'Y', ADMIN_USERNAME);
+
+        logger.debug(ar.getMsg());
+        assertTrue("Cannot set empty email for user", !ar.isSuccess());
+
+        ar = userService.store(this.TEST_USERNAME, "David", "Francis", "diff@email.com", "validpwd", 'Y', ADMIN_USERNAME);
+
+        logger.debug(ar.getMsg());
+        assertTrue("Assigning new email to user is allowed", ar.isSuccess());
+
+        logger.debug("\nFINISHED testAddNew()\n");
+    }
+
+    /**
+     * Test case for the remove(User) method of the UserService
+     * implementation
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testRemove() throws Exception {
+
+        String ADMIN_USERNAME = "osavruk";
+        
+        logger.debug("\nSTARTED testRemove()\n");
+
+        User user1 = userDao.find(TEST_USERNAME);
+
+        List<AssignmentSubmission> submissions = assignmentSubmissionDao.findUserSubmissions(user1);
+        Result<User> ar;
+
+        if (submissions.isEmpty()) {
+
+            ar = userService.remove(TEST_USERNAME, ADMIN_USERNAME);
+            logger.debug(ar.getMsg());
+            assertTrue("Delete of user should be allowed as no task logs assigned!", ar.isSuccess());
+
+        } else {
+
+            // this user has assignment submissions pending
+            ar = userService.remove(TEST_USERNAME, ADMIN_USERNAME);
+            logger.debug(ar.getMsg());
+            assertTrue("Cascading delete of user to assignment submissions not allowed!", !ar.isSuccess());
+
+        }
+        logger.debug("\nFINISHED testRemove()\n");        
+    }
+
+    @Test
+    public void testLogon() {
+
+        Result<User> ar = userService.findByUsernamePassword("vkotusenko", "admin");
+
+        assertTrue("Valid user could not be found for valid user/pwd", ar.getData() != null);
+        assertTrue(ar.isSuccess());
+
+        ar = userService.findByUsernamePassword("vkotusenko", "ADMIN");
+
+        assertTrue("Invalid logic - valid user found with UPPERCASE password", ar.getData() == null);
+        assertTrue(!ar.isSuccess());
+
+        ar = userService.findByUsernamePassword("kotus@kmbs.ua", "admin");
+
+        assertTrue("Valid user could not be found for valid email/pwd", ar.getData() != null);
+        assertTrue(ar.isSuccess());
+
+        ar = userService.findByUsernamePassword("vkotusenko", "invalidadmin");
+        assertTrue("Invalid user verified with wrong password", ar.getData() == null);
+        assertTrue(!ar.isSuccess());
+
+        ar = userService.findByUsernamePassword("blah", "blah");
+        assertTrue("Invalid user verified with wrong username and password", ar.getData() == null);
+        assertTrue(!ar.isSuccess());
+    }
+}
