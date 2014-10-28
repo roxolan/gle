d8ee24c817dc2e3ffda35b51df79ab80a7577660
Vic on Mac <roxolanus@gmail.com>
2014-10-29 01:51:37 +0200

    web/CourseHandler

---
+++ src/main/java/com/roxolanus/gle/web/CourseHandler.java
@@ -0,0 +1,130 @@
+package com.roxolanus.gle.web;
+
+import com.roxolanus.gle.domain.*;
+import com.roxolanus.gle.service.CourseService;
+import com.roxolanus.gle.vo.Result;
+import static com.roxolanus.gle.web.SecurityHelper.getSessionUser;
+
+import java.util.List;
+import javax.json.JsonObject;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/course")
+public class CourseHandler extends AbstractHandler {
+
+    @Autowired
+    protected CourseService courseService;
+    
+    @RequestMapping(value = "/find", method = RequestMethod.GET, produces = {"application/json"})
+    @ResponseBody
+    public String find(
+            @RequestParam(value = "idCourse", required = true) Integer idCourse,
+            HttpServletRequest request) {
+
+        User sessionUser = getSessionUser(request);
+        if (sessionUser == null) {
+            return getJsonErrorMsg("User is not logged on");
+        }
+        
+        Result<Course> ar = courseService.find(idCourse, sessionUser.getUsername());
+
+        if (ar.isSuccess()) {
+
+            return getJsonSuccessData(ar.getData());
+
+        } else {
+
+            return getJsonErrorMsg(ar.getMsg());
+
+        }
+    }
+
+    @RequestMapping(value = "/store", method = RequestMethod.POST, produces = {"application/json"})
+    @ResponseBody
+    public String store(
+            @RequestParam(value = "data", required = true) String jsonData,
+            HttpServletRequest request) {
+
+        User sessionUser = getSessionUser(request);
+        if (sessionUser == null) {
+            return getJsonErrorMsg("User is not logged on");
+        }
+        
+        JsonObject jsonObj = parseJsonObject(jsonData);
+        
+        Result<Course> ar = courseService.store(
+                getIntegerValue(jsonObj.get("idCourse")),
+                getIntegerValue(jsonObj.get("idLgroup")),
+                jsonObj.getString("courseTitle"),
+                sessionUser.getUsername());
+
+        if (ar.isSuccess()) {
+
+            return getJsonSuccessData(ar.getData());
+
+        } else {
+
+            return getJsonErrorMsg(ar.getMsg());
+
+        }
+    }
+
+    @RequestMapping(value = "/remove", method = RequestMethod.POST, produces = {"application/json"})
+    @ResponseBody
+    public String remove(
+            @RequestParam(value = "data", required = true) String jsonData,
+            HttpServletRequest request) {
+
+        User sessionUser = getSessionUser(request);
+        if (sessionUser == null) {
+            return getJsonErrorMsg("User is not logged on");
+        }
+        
+        JsonObject jsonObj = parseJsonObject(jsonData);
+
+        Result<Course> ar = courseService.remove(
+                getIntegerValue(jsonObj.get("idCourse")), 
+                sessionUser.getUsername());
+
+        if (ar.isSuccess()) {
+
+            return getJsonSuccessMsg(ar.getMsg());
+
+        } else {
+
+            return getJsonErrorMsg(ar.getMsg());
+
+        }
+    }
+
+    @RequestMapping(value = "/findAll", method = RequestMethod.GET, produces = {"application/json"})
+    @ResponseBody
+    public String findAll(
+            HttpServletRequest request) {
+
+        User sessionUser = getSessionUser(request);
+        if (sessionUser == null) {
+            return getJsonErrorMsg("User is not logged on");
+        }
+        
+        Result<List<Course>> ar = courseService.findAll(sessionUser.getUsername());
+
+        if (ar.isSuccess()) {
+
+            return getJsonSuccessData(ar.getData());
+
+        } else {
+
+            return getJsonErrorMsg(ar.getMsg());
+
+        }
+    }
+}
