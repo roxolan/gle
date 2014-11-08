3a76d9d46bf200d09d06f1614ce7967e89a82159
Vic on Mac <roxolanus@gmail.com>
2014-11-08 16:44:31 +0200

    community board left panel menu refactored to be included within the panel itself

--- src/main/webapp/app/view/community/board/left/panel/CommunityBoardLeftPanel.js
+++ src/main/webapp/app/view/community/board/left/panel/CommunityBoardLeftPanel.js
@@ -1,22 +1,50 @@
 Ext.define('Kmbsvle.view.community.board.left.panel.CommunityBoardLeftPanel', {
-  extend: 'Ext.panel.Panel',
+    extend: 'Ext.panel.Panel',
     
-  alias: 'widget.community.board.left.panel',
-  controller: 'community.board.left.panel',
-  viewModel: {
-    type: 'community.board.left.panel'
-  },
-  requires: [
-    'Kmbsvle.view.community.board.left.panel.CommunityBoardLeftPanelController',
-    'Kmbsvle.view.community.board.left.panel.CommunityBoardLeftPanelModel',
-    'Kmbsvle.view.community.board.left.panel.menu.CommunityBoardLeftPanelMenu'
-  ],
-  header: false,
-  width: 250,
-  items: [
-    {
-      xtype: 'community.board.left.panel.menu'
-    }
-  ]
-  
+    alias: 'widget.community.board.left.panel',
+    controller: 'community.board.left.panel',
+    viewModel: {
+        type: 'community.board.left.panel'
+    },
+    requires: [
+        'Kmbsvle.view.community.board.left.panel.CommunityBoardLeftPanelController',
+        'Kmbsvle.view.community.board.left.panel.CommunityBoardLeftPanelModel'
+    ],
+    header: false,
+    width: 250,
+    items: [
+        {
+            xtype: 'menu',
+            floating: false,
+            items: [
+                { 
+                    xtype: 'menuitem',
+                    text: 'Спільнота №1',
+                    listeners: {
+                        click: 'openCommunitySingle'
+                    }
+                },
+                { 
+                    xtype: 'menuseparator' 
+                },
+                { 
+                    xtype: 'menuitem',
+                    text: 'Спільнота №2',
+                    listeners: {
+                        click: 'openCommunitySingle'
+                    }
+                },
+                { 
+                    xtype: 'menuseparator' 
+                },
+                { 
+                    xtype: 'menuitem',
+                    text: 'Спільнота №3',
+                    listeners: {
+                        click: 'openCommunitySingle'
+                    }
+                }
+            ]
+        }
+    ]
 });
--- src/main/webapp/app/view/community/board/left/panel/CommunityBoardLeftPanelController.js
+++ src/main/webapp/app/view/community/board/left/panel/CommunityBoardLeftPanelController.js
@@ -1,7 +1,26 @@
 Ext.define('Kmbsvle.view.community.board.left.panel.CommunityBoardLeftPanelController', {
-  extend: 'Kmbsvle.view.base.BaseViewController',
+    extend: 'Kmbsvle.view.base.BaseViewController',
 
-  alias: 'controller.community.board.left.panel'
+    alias: 'controller.community.board.left.panel',
+  
+    requires: [
+        'Kmbsvle.view.community.single.left.panel.CommunitySingleLeftPanel',
+        'Kmbsvle.view.community.single.center.panel.CommunitySingleCenterPanel',
+        'Kmbsvle.view.community.single.right.panel.CommunitySingleRightPanel'
+    ],
+
+    routes: {
+        'community': 'openCommunitySingle'
+    },
+
+
+    openCommunitySingle: function() {
+        var me = this;
+        me.changeLeftPanel('widget.community.single.left.panel', 'community_single_left_panel_title', 'expand');
+        me.changeCenterPanel('widget.community.single.center.panel', 'community_single_center_panel_title');
+        me.changeRightPanel('widget.community.single.right.panel', 'community_single_right_panel_title', 'collapse');
+        me.redirectTo('community');
+    }
 
 
 }); 
