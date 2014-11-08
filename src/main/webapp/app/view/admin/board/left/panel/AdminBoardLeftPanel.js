483089683fa10b9a393f969cb9c009fae2636b7c
Vic on Mac <roxolanus@gmail.com>
2014-11-08 15:56:28 +0200

    tabbed to the right so that 4 spaces are used consistently

--- src/main/webapp/app/view/admin/board/left/panel/AdminBoardLeftPanel.js
+++ src/main/webapp/app/view/admin/board/left/panel/AdminBoardLeftPanel.js
@@ -1,53 +1,53 @@
 Ext.define('Kmbsvle.view.admin.board.left.panel.AdminBoardLeftPanel', {
-  extend: 'Ext.panel.Panel',
+    extend: 'Ext.panel.Panel',
   
-  alias: 'widget.admin.board.left.panel',
-  controller: 'admin.board.left.panel',
-  viewModel: {
-    type: 'admin.board.left.panel'
-  },
-  requires: [
-    'Kmbsvle.view.admin.board.left.panel.AdminBoardLeftPanelController',
-    'Kmbsvle.view.admin.board.left.panel.AdminBoardLeftPanelModel'
-  ],
-  header: false,
-  width: 250,
-  items: [
-    {
-        xtype: 'menu',
-        floating: false,
-        items: [
-            {
-                xtype: 'menuitem',
-                reference: 'admin_board_auth_board',
-                text: 'Автентифікація',
-                listeners: {
-                    click: 'openAuthBoard'
+    alias: 'widget.admin.board.left.panel',
+    controller: 'admin.board.left.panel',
+    viewModel: {
+        type: 'admin.board.left.panel'
+    },
+    requires: [
+        'Kmbsvle.view.admin.board.left.panel.AdminBoardLeftPanelController',
+        'Kmbsvle.view.admin.board.left.panel.AdminBoardLeftPanelModel'
+    ],
+    header: false,
+    width: 250,
+    items: [
+        {
+            xtype: 'menu',
+            floating: false,
+            items: [
+                {
+                    xtype: 'menuitem',
+                    reference: 'admin_board_auth_board',
+                    text: 'Автентифікація',
+                    listeners: {
+                        click: 'openAuthBoard'
+                    }
+                },
+                { 
+                    xtype: 'menuseparator' 
+                },
+                { 
+                    xtype: 'menuitem',
+                    reference: 'admin_board_group_board',
+                    text: 'Групи',
+                    listeners: {
+                        click: 'openGroupBoard'
+                    }
+                },
+                { 
+                    xtype: 'menuseparator' 
+                },
+                { 
+                    xtype: 'menuitem',
+                    reference: 'admin_board_library_board',
+                    text: 'Бібліотека',
+                    listeners: {
+                        click: 'openLibraryBoard'
+                    }
                 }
-            },
-            { 
-                xtype: 'menuseparator' 
-            },
-            { 
-                xtype: 'menuitem',
-                reference: 'admin_board_group_board',
-                text: 'Групи',
-                listeners: {
-                    click: 'openGroupBoard'
-                }
-            },
-            { 
-                xtype: 'menuseparator' 
-            },
-            { 
-                xtype: 'menuitem',
-                reference: 'admin_board_library_board',
-                text: 'Бібліотека',
-                listeners: {
-                    click: 'openLibraryBoard'
-                }
-            }
-        ]
-    }
-  ]
+            ]
+        }
+    ]
 });
