ece292e7990ecd462a11d6d2d7b2078123024277
Vic on Mac <roxolanus@gmail.com>
2014-11-08 17:14:15 +0200

    community single left panel menu refactored to be included in the panel itself

--- src/main/webapp/app/view/community/single/left/panel/CommunitySingleLeftPanel.js
+++ src/main/webapp/app/view/community/single/left/panel/CommunitySingleLeftPanel.js
@@ -1,22 +1,30 @@
 Ext.define('Kmbsvle.view.community.single.left.panel.CommunitySingleLeftPanel', {
-  extend: 'Ext.panel.Panel',
+    extend: 'Ext.panel.Panel',
     
-  alias: 'widget.community.single.left.panel',
-  controller: 'community.single.left.panel',
-  viewModel: {
-    type: 'community.single.left.panel'
-  },
-  requires: [
-    'Kmbsvle.view.community.single.left.panel.CommunitySingleLeftPanelController',
-    'Kmbsvle.view.community.single.left.panel.CommunitySingleLeftPanelModel',
-    'Kmbsvle.view.community.single.left.panel.menu.CommunitySingleLeftPanelMenu'
-
-  ],
-  header: false,
-  width: 250,
-  items: [
-      {
-        xtype: 'community.single.left.panel.menu'
-      }
-  ]
+    alias: 'widget.community.single.left.panel',
+    controller: 'community.single.left.panel',
+    viewModel: {
+        type: 'community.single.left.panel'
+    },
+    requires: [
+        'Kmbsvle.view.community.single.left.panel.CommunitySingleLeftPanelController',
+        'Kmbsvle.view.community.single.left.panel.CommunitySingleLeftPanelModel'
+    ],
+    header: false,
+    width: 250,
+    items: [
+        {
+            xtype: 'menu',
+            floating: false,
+            items: [
+                { 
+                    xtype: 'menuitem',
+                    text: 'Події спільноти',
+                    listeners: {
+                        click: 'openEventsBoard'
+                    }
+                }
+            ]
+        }
+    ]
 });
--- src/main/webapp/app/view/community/single/left/panel/CommunitySingleLeftPanelController.js
+++ src/main/webapp/app/view/community/single/left/panel/CommunitySingleLeftPanelController.js
@@ -1,11 +1,25 @@
 Ext.define('Kmbsvle.view.community.single.left.panel.CommunitySingleLeftPanelController', {
-  extend: 'Kmbsvle.view.base.BaseViewController',
+    extend: 'Kmbsvle.view.base.BaseViewController',
 
-  alias: 'controller.community.single.left.panel',
+    alias: 'controller.community.single.left.panel',
 
-  requires: [
-    'Kmbsvle.view.community.single.left.panel.menu.CommunitySingleLeftPanelMenuController'
-  ]
+    requires: [
+        'Kmbsvle.view.event.board.left.panel.EventBoardLeftPanel',
+        'Kmbsvle.view.event.board.center.panel.EventBoardCenterPanel',
+        'Kmbsvle.view.event.board.right.panel.EventBoardRightPanel'
+    ],
+
+    routes: {
+        'community/events' : 'openEventsBoard'
+    },
+
+    openEventsBoard: function() {
+        var me = this;
+        me.changeLeftPanel('widget.event.board.left.panel', 'event_board_left_panel_title', 'collapse');
+        me.changeCenterPanel('widget.event.board.center.panel', 'event_board_center_panel_title');
+        me.changeRightPanel('widget.event.board.right.panel', 'event_board_right_panel_title', 'expand');
+        me.redirectTo('community/events');
+    }
 
 
 }); 
