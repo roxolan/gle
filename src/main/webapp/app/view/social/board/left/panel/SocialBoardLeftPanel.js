3789ef2b6fceab9b59ce8127bca7e765d5a41e1c
Vic on Mac <roxolanus@gmail.com>
2014-11-09 17:53:08 +0200

    social board left panel menu refactored to be within the panel itself

--- src/main/webapp/app/view/social/board/left/panel/SocialBoardLeftPanel.js
+++ src/main/webapp/app/view/social/board/left/panel/SocialBoardLeftPanel.js
@@ -1,21 +1,60 @@
 Ext.define('Kmbsvle.view.social.board.left.panel.SocialBoardLeftPanel', {
-  extend: 'Ext.panel.Panel',
+    extend: 'Ext.panel.Panel',
     
-  alias: 'widget.social.board.left.panel',
-  controller: 'social.board.left.panel',
-  viewModel: {
-    type: 'social.board.left.panel'
-  },
-  requires: [
-    'Kmbsvle.view.social.board.left.panel.SocialBoardLeftPanelController',
-    'Kmbsvle.view.social.board.left.panel.SocialBoardLeftPanelModel',
-    'Kmbsvle.view.social.board.left.panel.menu.SocialBoardLeftPanelMenu'
-  ],
-  header: false,
-  width: 250,
-  items: [
-      {
-        xtype: 'social.board.left.panel.menu'
-      }
-  ]
+    alias: 'widget.social.board.left.panel',
+    controller: 'social.board.left.panel',
+    viewModel: {
+        type: 'social.board.left.panel'
+    },
+    requires: [
+        'Kmbsvle.view.social.board.left.panel.SocialBoardLeftPanelController',
+        'Kmbsvle.view.social.board.left.panel.SocialBoardLeftPanelModel'
+    ],
+    header: false,
+    width: 250,
+    items: [
+        {
+            xtype: 'menu',
+            floating: false,
+            items: [
+                {
+                    xtype: 'menuitem',
+                    text: 'Спільнота №1',
+                    listeners: {
+                        click: 'openCommunitySingle'
+                    }
+                },
+                { 
+                    xtype: 'menuseparator' 
+                },
+                {
+                    xtype: 'menuitem',
+                    text: 'Спільнота №2',
+                    listeners: {
+                        click: 'openCommunitySingle'
+                    }
+                },
+                { 
+                    xtype: 'menuseparator' 
+                },
+                {
+                    xtype: 'menuitem',
+                    text: 'Спільнота №3',
+                    listeners: {
+                        click: 'openCommunitySingle'
+                    }
+                },
+                { 
+                    xtype: 'menuseparator' 
+                },
+                {
+                    xtype: 'menuitem',
+                    text: 'Мої спільноти',
+                    listeners: {
+                        click: 'openCommunityBoard'
+                    }
+                }
+            ]
+        }
+    ]
 });
--- src/main/webapp/app/view/social/board/left/panel/SocialBoardLeftPanelController.js
+++ src/main/webapp/app/view/social/board/left/panel/SocialBoardLeftPanelController.js
@@ -1,7 +1,37 @@
 Ext.define('Kmbsvle.view.social.board.left.panel.SocialBoardLeftPanelController', {
-  extend: 'Kmbsvle.view.base.BaseViewController',
+    extend: 'Kmbsvle.view.base.BaseViewController',
 
-  alias: 'controller.social.board.left.panel'
+    alias: 'controller.social.board.left.panel',
+  
+    requires: [
+        'Kmbsvle.view.community.board.left.panel.CommunityBoardLeftPanel',
+        'Kmbsvle.view.community.board.center.panel.CommunityBoardCenterPanel',
+        'Kmbsvle.view.community.board.right.panel.CommunityBoardRightPanel',
 
+        'Kmbsvle.view.community.single.left.panel.CommunitySingleLeftPanel',
+        'Kmbsvle.view.community.single.center.panel.CommunitySingleCenterPanel',
+        'Kmbsvle.view.community.single.right.panel.CommunitySingleRightPanel'
+    ],
 
+    routes: {
+        'community/board': 'openCommunityBoard',
+        'community/1': 'openCommunitySingle'
+    },
+
+    openCommunitySingle: function() {
+        var me = this;
+        me.changeLeftPanel('widget.community.single.left.panel', 'community_single_left_panel_title', 'expand');
+        me.changeCenterPanel('widget.community.single.center.panel');
+        me.changeRightPanel('widget.community.single.right.panel', 'community_single_right_panel_title', 'collapse');
+        me.redirectTo('community/1');
+    },
+
+    openCommunityBoard: function() {
+        var me = this;
+        me.changeLeftPanel('widget.community.board.left.panel', 'community_board_left_panel_title', 'expand');
+        me.changeCenterPanel('widget.community.board.center.panel');
+        me.changeRightPanel('widget.community.board.right.panel', 'community_board_right_panel_title', 'collapse');
+        me.redirectTo('community/board');
+    }
+    
 }); 
