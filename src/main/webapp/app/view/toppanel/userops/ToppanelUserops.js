efa9dbd058d2ef69727ac934f0bf796cd88967fb
Vic on Mac <roxolanus@gmail.com>
2014-11-09 18:59:30 +0200

    toppanel userops menu refactored also to one level

--- src/main/webapp/app/view/toppanel/userops/ToppanelUserops.js
+++ src/main/webapp/app/view/toppanel/userops/ToppanelUserops.js
@@ -1,26 +1,40 @@
 Ext.define('Kmbsvle.view.toppanel.userops.ToppanelUserops', {
-  extend: 'Ext.panel.Panel',
+    extend: 'Ext.panel.Panel',
   
-  alias: 'widget.toppanel.userops',
-  requires: [
-      'Kmbsvle.view.toppanel.userops.ToppanelUseropsController',
-      'Kmbsvle.view.toppanel.userops.ToppanelUseropsModel',
-      'Kmbsvle.view.toppanel.userops.menu.ToppanelUseropsMenu'
-  ],
+    alias: 'widget.toppanel.userops',
+    requires: [
+        'Kmbsvle.view.toppanel.userops.ToppanelUseropsController',
+        'Kmbsvle.view.toppanel.userops.ToppanelUseropsModel'
+    ],
   
-  controller: 'toppanel.userops',
-  viewModel: {
-    type: 'toppanel.userops'
-  },
-  padding: '0 0 0 0',
-  margin: '4 30 0 0',
-  height: 32,
-  width: 210,
-  overCls: 'hoveronname',
-  items: {
-    xtype: 'toppanel.userops.menu'
-  }
-
-  
-
+    controller: 'toppanel.userops',
+    viewModel: {
+        type: 'toppanel.userops'
+    },
+    padding: '0 0 0 0',
+    margin: '4 30 0 0',
+    height: 32,
+    width: 210,
+    overCls: 'hoveronname',
+    items: {
+        xtype: 'menu',
+        width: 210,
+        id: 'toppaneluseropsmenu',      
+        floating: false,
+        layout: {
+            type: 'hbox',
+            pack: 'end'
+        },
+        items: [
+            { 
+                xtype: 'menuitem',
+                reference: 'userops_username',
+                glyph: 'xf007@FontAwesome',
+                listeners: {
+                    click: 'openProfileEdit'
+                }
+            }
+        ]
+    }
+    
 });
--- src/main/webapp/app/view/toppanel/userops/ToppanelUseropsController.js
+++ src/main/webapp/app/view/toppanel/userops/ToppanelUseropsController.js
@@ -1,19 +1,28 @@
 Ext.define('Kmbsvle.view.toppanel.userops.ToppanelUseropsController', {
-  extend: 'Kmbsvle.view.base.BaseViewController',
+    extend: 'Kmbsvle.view.base.BaseViewController',
 
-  alias: 'controller.toppanel.userops',
+    alias: 'controller.toppanel.userops',
+    
+    requires: [
+        'Kmbsvle.view.profile.form.Edit'
+    ],
+  
+    openProfileEdit: function() {
+        Ext.create('widget.profile.form.edit');
+    },
 
-  openProfile: function() {
-    Ext.Msg.alert('Controller in the same Folder!');
-  }
-/*
-  control: {
-    '#': {
-      click: 'openProfile'
+    setUserName: function() {
+        var me = this;
+        var menuitem = me.lookupReference('userops_username');
+        var fullname = Kmbsvle.getApplication().getUser().fullName;
+    
+        menuitem.setConfig('text', fullname);
+    },
+
+    control: {
+        '#': {
+            afterrender: 'setUserName'
+        }
     }
-  }
-*/
-
-
 
 }); 
