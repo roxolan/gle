ceea34c44e760b97f7a097bed865a360000c35ca
Vic on Mac <roxolanus@gmail.com>
2014-11-09 12:32:13 +0200

    group board left panel menu refactored to be included in the panel itself

--- src/main/webapp/app/view/group/board/left/panel/GroupBoardLeftPanel.js
+++ src/main/webapp/app/view/group/board/left/panel/GroupBoardLeftPanel.js
@@ -1,22 +1,50 @@
 Ext.define('Kmbsvle.view.group.board.left.panel.GroupBoardLeftPanel', {
-  extend: 'Ext.panel.Panel',
+    extend: 'Ext.panel.Panel',
   
-  alias: 'widget.group.board.left.panel',
-  controller: 'group.board.left.panel',
-  viewModel: {
-    type: 'group.board.left.panel'
-  },
-  requires: [
-    'Kmbsvle.view.group.board.left.panel.GroupBoardLeftPanelController',
-    'Kmbsvle.view.group.board.left.panel.GroupBoardLeftPanelModel',
-    'Kmbsvle.view.group.board.left.panel.menu.GroupBoardLeftPanelMenu'
-  ],
-  header: false,
-  width: 250,
-  items: [
-    {
-      xtype: 'group.board.left.panel.menu'
-    }
-  ]
-
+    alias: 'widget.group.board.left.panel',
+    controller: 'group.board.left.panel',
+    viewModel: {
+        type: 'group.board.left.panel'
+    },
+    requires: [
+        'Kmbsvle.view.group.board.left.panel.GroupBoardLeftPanelController',
+        'Kmbsvle.view.group.board.left.panel.GroupBoardLeftPanelModel'
+    ],
+    header: false,
+    width: 250,
+    items: [
+        {
+            xtype: 'menu',
+            floating: false,
+            items: [
+                { 
+                    xtype: 'menuitem',
+                    text: 'Група №1',
+                    listeners: {
+                        click: 'openGroupSingle'
+                    }
+                },
+                { 
+                    xtype: 'menuseparator' 
+                },
+                { 
+                    xtype: 'menuitem',
+                    text: 'Група №2',
+                    listeners: {
+                        click: 'openGroupSingle'
+                    }
+                },
+                { 
+                    xtype: 'menuseparator' 
+                },
+                { 
+                    xtype: 'menuitem',
+                    text: 'Група №3',
+                    listeners: {
+                        click: 'openGroupSingle'
+                    }
+                }
+            ]
+        }
+    ]
 });
