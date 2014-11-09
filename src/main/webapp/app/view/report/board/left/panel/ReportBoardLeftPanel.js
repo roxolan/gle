c91bd7f2d11c8eac57b35e6f54caf05d2a82b2ed
Vic on Mac <roxolanus@gmail.com>
2014-11-09 15:51:05 +0200

    report board left panel menu refactored to be within the panel itself

--- src/main/webapp/app/view/report/board/left/panel/ReportBoardLeftPanel.js
+++ src/main/webapp/app/view/report/board/left/panel/ReportBoardLeftPanel.js
@@ -1,22 +1,50 @@
 Ext.define('Kmbsvle.view.report.board.left.panel.ReportBoardLeftPanel', {
-  extend: 'Ext.panel.Panel',
+    extend: 'Ext.panel.Panel',
   
-  alias: 'widget.report.board.left.panel',
-  controller: 'report.board.left.panel',
-  viewModel: {
-    type: 'report.board.left.panel'
-  },
-  requires: [
-    'Kmbsvle.view.report.board.left.panel.ReportBoardLeftPanelController',
-    'Kmbsvle.view.report.board.left.panel.ReportBoardLeftPanelModel',
-    'Kmbsvle.view.report.board.left.panel.menu.ReportBoardLeftPanelMenu'
-  ],
-  header: false,
-  width: 250,
-  items: [
-    {
-      xtype: 'report.board.left.panel.menu'
-    }
-  ]
-
+    alias: 'widget.report.board.left.panel',
+    controller: 'report.board.left.panel',
+    viewModel: {
+        type: 'report.board.left.panel'
+    },
+    requires: [
+        'Kmbsvle.view.report.board.left.panel.ReportBoardLeftPanelController',
+        'Kmbsvle.view.report.board.left.panel.ReportBoardLeftPanelModel'
+    ],
+    header: false,
+    width: 250,
+    items: [
+        {
+            xtype: 'menu',
+            floating: false,
+            items: [
+                { 
+                    xtype: 'menuitem',
+                    text: 'Звіт про учасника №1',
+                    listeners: {
+                        click: 'openReportSingle'
+                    }
+                },
+                { 
+                    xtype: 'menuseparator' 
+                },
+                { 
+                    xtype: 'menuitem',
+                    text: 'Звіт про учасника №2',
+                    listeners: {
+                        click: 'openReportSingle'
+                    }
+                },
+                { 
+                    xtype: 'menuseparator' 
+                },
+                { 
+                    xtype: 'menuitem',
+                    text: 'Звіт про учасника №3',
+                    listeners: {
+                        click: 'openReportSingle'
+                    }
+                }         
+            ]
+        }
+    ]
 });
--- src/main/webapp/app/view/report/board/left/panel/ReportBoardLeftPanelController.js
+++ src/main/webapp/app/view/report/board/left/panel/ReportBoardLeftPanelController.js
@@ -1,7 +1,25 @@
 Ext.define('Kmbsvle.view.report.board.left.panel.ReportBoardLeftPanelController', {
-  extend: 'Kmbsvle.view.base.BaseViewController',
+    extend: 'Kmbsvle.view.base.BaseViewController',
 
-  alias: 'controller.report.board.left.panel'
+    alias: 'controller.report.board.left.panel',
+  
+    requires: [
+        'Kmbsvle.view.report.single.left.panel.ReportSingleLeftPanel',
+        'Kmbsvle.view.report.single.center.panel.ReportSingleCenterPanel',
+        'Kmbsvle.view.report.single.right.panel.ReportSingleRightPanel'
+    ],
 
+    routes: {
+        'report/1' : 'openReportSingle'
+    },
+
+
+    openReportSingle: function() {
+        var me = this;
+        me.changeLeftPanel('widget.report.single.left.panel', 'report_single_left_panel_title', 'expand');
+        me.changeCenterPanel('widget.report.single.center.panel');
+        me.changeRightPanel('widget.report.single.right.panel', 'report_single_right_panel_title', 'collapse');
+        me.redirectTo('report/1');
+    }
 
 }); 
