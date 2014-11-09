c81f16c19a86658a88cb292a19f3130e40f8ffcb
Vic on Mac <roxolanus@gmail.com>
2014-11-09 19:53:33 +0200

    idiomatic use of initComponent() on dashboard central panel

--- src/main/webapp/app/view/dashboard/center/panel/DashboardCenterPanel.js
+++ src/main/webapp/app/view/dashboard/center/panel/DashboardCenterPanel.js
@@ -3,6 +3,7 @@ Ext.define('Kmbsvle.view.dashboard.center.panel.DashboardCenterPanel', {
 
     alias: 'widget.dashboard.center.panel',
     requires: [
+        'Ext.XTemplate',
         'Kmbsvle.view.dashboard.center.panel.DashboardCenterPanelController',
         'Kmbsvle.view.dashboard.center.panel.DashboardCenterPanelModel'
     ],
@@ -12,13 +13,28 @@ Ext.define('Kmbsvle.view.dashboard.center.panel.DashboardCenterPanel', {
     },
     autoScroll: true,
     bodyPadding: 10,
-    items:[{
-        title: 'Навчання',
-        html: '<h2>First tab content</h2>'
-    },
-    {
-        title: ' Спільнота',
-        html: '<h3>A bit smaller heading, #3</h3>'
-    }]
-
+    
+    initComponent: function() {
+        var me = this;
+        var tpl = new Ext.XTemplate(
+                '<tpl for=".">', 
+                '<p>Welcome, <b>{fullName}</b>!</p>',
+                '</tpl>'); 
+                
+        var welcomeHtml = tpl.apply(Kmbsvle.getApplication().getUser());
+        
+        Ext.applyIf(me, {
+            items: [
+                {
+                    title: 'Навчання',
+                    html: welcomeHtml
+                },
+                {
+                    title: ' Спільнота',
+                    html: '<h3>A bit smaller heading, #3</h3>'
+                }
+            ]
+        });
+        me.callParent(arguments);            
+    }
 });
