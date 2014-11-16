d8ed4b44811285768e3abb61b3b61fa3f2bc3b1d
Vic on Mac <roxolanus@gmail.com>
2014-11-16 12:35:05 +0200

    loading a store should be done with a callback

--- src/main/webapp/app/view/auth/board/center/userlist/AuthBoardCenterUserlistController.js
+++ src/main/webapp/app/view/auth/board/center/userlist/AuthBoardCenterUserlistController.js
@@ -5,7 +5,15 @@ Ext.define('Kmbsvle.view.auth.board.center.userlist.AuthBoardCenterUserlistContr
     
     initStore: function() {
         var me = this;
-        me.getStore('users').load();
+        var userlist = me.getStore('users');
+        userlist.load(function(records, operation, success){
+            Kmbsvle.console(records);
+            Kmbsvle.console(operation);
+            Kmbsvle.console(success);
+        });
+        
+        // var firstRow = userlist.getData()[0];
+        // Kmbsvle.console(firstRow);
     }
 
 
