2dd7b4a133877455f46dda65d2b88ca40610edb7
Vic on Mac <roxolanus@gmail.com>
2014-11-27 07:08:18 +0200

    tried record.destroy() again - even after disabling autoSync and autoLoad for central panel user list, it does not get through (looks like it is not even used), so using direct Ext.Ajax.request which works

--- src/main/webapp/app/view/auth/board/right/userform/AuthBoardRightUserformController.js
+++ src/main/webapp/app/view/auth/board/right/userform/AuthBoardRightUserformController.js
@@ -5,7 +5,7 @@ Ext.define('Kmbsvle.view.auth.board.right.userform.AuthBoardRightUserformControl
     
     initStore: function(view, eOpts) {
         var me = this;
-        me.getStore('user').load(function(records, operation, success){
+        me.getStore('userlist').load(function(records, operation, success){
         });
     },
     
@@ -13,21 +13,33 @@ Ext.define('Kmbsvle.view.auth.board.right.userform.AuthBoardRightUserformControl
         var me = this;
 //        me.getStore('user').load(function(records, operation, success){
 //        });
+
         var form = me.getView().getForm();
         var record = form.getRecord();        
-        form.updateRecord();
 
         // record.data = form.getFieldValues();
-        
-        
-        var errs = record.validate();
-        if (errs.isValid()) {
+        if (!Ext.isEmpty(record)) {
+            me.getView().getForm().updateRecord();
+            var errs = record.validate();
+            if (errs.isValid()) {
             
             // Kmbsvle.console(record);
             // Kmbsvle.console(record[0]);
-            
-            try {
+
+            //try {
+//                Ext.Ajax.request({
+//                    url: 'user/store.json',
+//                    params: {
+//                        data: Ext.JSON.encode(record.data)
+//                    },
+//                    success: function() {
+//                        me.refreshStore();
+//                    }
+//                });
                 record.save({
+//                    extraParams: {
+//                        data: Ext.JSON.encode(record.data)
+//                    },
                     success: function(record, operation) {
                         if (typeof record.store === 'undefined') {
                             Ext.getCmp('centerholder').getComponent('centerUserList').getStore().add(record);
@@ -38,12 +50,13 @@ Ext.define('Kmbsvle.view.auth.board.right.userform.AuthBoardRightUserformControl
                         Ext.Msg.alert('Save Failure', operation.request.scope.reader.jsonData.msg);
                     }
                 });
-            } catch (ConstraintViolationException) {
-                throw new MyException("can not save record: ", e);
-            }
-        } else {
-            form.markInvalid(errs);
-            Ext.Msg.alert('Invalid Fields', 'Please fix the invalid entries!');
+//            } catch (ConstraintViolationException) {
+//                throw new ConstraintViolationException("can not save record: ", e);
+//            }
+            } else {
+                form.markInvalid(errs);
+                Ext.Msg.alert('Invalid Fields', 'Please fix the invalid entries!');
+            }           
         }
         // Kmbsvle.console(record);
     },
@@ -70,14 +83,17 @@ Ext.define('Kmbsvle.view.auth.board.right.userform.AuthBoardRightUserformControl
                                 me.refreshStore();
                             }
                         });
-//                        record.delete({
-//                            success: function(record, operation) {
-//                                Kmbsvle.console(operation);
-//                            },
-//                            failure: function(record, operation) {
-//                                Ext.Msg.alert('Не вдалося видалити', operation.request.scope.reader.jsonData.msg);
-//                            }
-//                        });
+                    /* 
+                        FOR SOME REASON??? .destroy() does not get through, so using direct Ext.Ajax.request() as above
+                        record.destroy({
+                            success: function(record, operation) {
+                                Kmbsvle.console(operation);
+                            },
+                            failure: function(record, operation) {
+                                Ext.Msg.alert('Не вдалося видалити', operation.request.scope.reader.jsonData.msg);
+                            }
+                        });
+                    */
                     } catch (ConstraintViolationException) {
                         throw new ConstraintViolationException("can not delete record: ", e);
                     }
