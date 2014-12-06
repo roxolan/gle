1600d6d2de5edaf90d128e910db54a67dbafe413
Vic on Mac <roxolanus@gmail.com>
2014-12-06 09:33:18 +0200

    'auth.board.right.panel' was refactored to 'auth.board.right.userform', so it is not necessary anymore

--- src/main/webapp/app/view/auth/board/right/panel/AuthBoardRightPanelController.js
+++
@@ -1,7 +0,0 @@
-Ext.define('Kmbsvle.view.auth.board.right.panel.AuthBoardRightPanelController', {
-  extend: 'Kmbsvle.view.base.BaseViewController',
-
-  alias: 'controller.auth.board.right.panel'
-
-
-}); 
