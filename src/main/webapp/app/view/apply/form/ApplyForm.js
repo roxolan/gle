1f35f28064302b304cffe1782f890cddf74de0cb
Vic on Mac <roxolanus@gmail.com>
2014-12-19 20:15:22 +0200

    apply.form improved further a bit

--- src/main/webapp/app/view/apply/form/ApplyForm.js
+++ src/main/webapp/app/view/apply/form/ApplyForm.js
@@ -29,14 +29,14 @@ Ext.define('Kmbsvle.view.apply.form.ApplyForm', {
         {
             layout: {
                 type: 'hbox',
-                pack: 'end'
+                pack: 'start'
             },
             items: [
                 {
                     xtype: 'fieldset',
                     title: 'Контактна інформація',
                     defaultType: 'textfield',
-                    width: 600,
+                    width: 605,
                     layout: 'anchor',
                     defaults: {
                         anchor: '100%'
@@ -92,68 +92,68 @@ Ext.define('Kmbsvle.view.apply.form.ApplyForm', {
                                 {
                                     fieldLabel: 'Робочий email',
                                     name: 'email',
-                            vtype: 'email',
-                            flex: 1,
-                        }, 
+                                    vtype: 'email',
+                                    flex: 1,
+                                }, 
+                                {
+                                    fieldLabel: 'Моб. телефон',
+                                    // labelWidth: 100,
+                                    name: 'phone',
+                                    width: 200,
+                                    emptyText: 'xxx-xxx-xxxx',
+                                    maskRe: /[\d\-]/,
+                                    regex: /^\d{3}-\d{3}-\d{4}$/,
+                                    regexText: 'Повинен мати форматs   xxx-xxx-xxxx'
+                                }
+                            ]
+                        },
                         {
-                            fieldLabel: 'Моб. телефон',
-                            // labelWidth: 100,
-                            name: 'phone',
-                            width: 200,
-                            emptyText: 'xxx-xxx-xxxx',
-                            maskRe: /[\d\-]/,
-                            regex: /^\d{3}-\d{3}-\d{4}$/,
-                            regexText: 'Повинен мати форматs   xxx-xxx-xxxx'
+                            xtype: 'fieldcontainer',
+                            layout: 'hbox',
+                            defaultType: 'textfield',
+                            margin: '0 0 5 0',
+                            items: [
+                                {
+                                    fieldLabel: 'Особистий email',
+                                    name: 'email',
+                                    vtype: 'email',
+                                    flex: 1,
+                                }, 
+                                {
+                                    fieldLabel: 'Роб. телефон',
+                                    // labelWidth: 100,
+                                    name: 'phone',
+                                    width: 200,
+                                    emptyText: 'xxx-xxx-xxxx',
+                                    maskRe: /[\d\-]/,
+                                    regex: /^\d{3}-\d{3}-\d{4}$/,
+                                    regexText: 'Повинен мати формат   xxx-xxx-xxxx'
+                                }
+                            ]
+                        },
+                        {
+                            xtype: 'fieldcontainer',
+                            layout: {
+                                type: 'hbox',
+                                pack: 'end'
+                            },
+                            defaultType: 'textfield',
+                            margin: '0 0 5 0',
+                            items: [ 
+                                {
+                                    fieldLabel: 'Дом. телефон',
+                                    // labelWidth: 100,
+                                    name: 'phone',
+                                    width: 200,
+                                    emptyText: 'xxx-xxx-xxxx',
+                                    maskRe: /[\d\-]/,
+                                    regex: /^\d{3}-\d{3}-\d{4}$/,
+                                    regexText: 'Повинен мати формат   xxx-xxx-xxxx'
+                                }
+                            ]
                         }
                     ]
-                },
-                {
-                    xtype: 'fieldcontainer',
-                    layout: 'hbox',
-                    defaultType: 'textfield',
-                    margin: '0 0 5 0',
-                    items: [
-                        {
-                            fieldLabel: 'Особистий email',
-                            name: 'email',
-                            vtype: 'email',
-                            flex: 1,
-                        }, 
-                        {
-                            fieldLabel: 'Роб. телефон',
-                            // labelWidth: 100,
-                            name: 'phone',
-                            width: 200,
-                            emptyText: 'xxx-xxx-xxxx',
-                            maskRe: /[\d\-]/,
-                            regex: /^\d{3}-\d{3}-\d{4}$/,
-                            regexText: 'Повинен мати формат   xxx-xxx-xxxx'
-                        }
-                    ]
-                },
-                {
-                    xtype: 'fieldcontainer',
-                    layout: {
-                        type: 'hbox',
-                        pack: 'end'
-                    },
-                    defaultType: 'textfield',
-                    margin: '0 0 5 0',
-                    items: [ 
-                        {
-                            fieldLabel: 'Дом. телефон',
-                            // labelWidth: 100,
-                            name: 'phone',
-                            width: 200,
-                            emptyText: 'xxx-xxx-xxxx',
-                            maskRe: /[\d\-]/,
-                            regex: /^\d{3}-\d{3}-\d{4}$/,
-                            regexText: 'Повинен мати формат   xxx-xxx-xxxx'
-                        }
-                    ]
-                }
-            ]
-        },                 
+                },                 
                 {
                     xtype: 'image',
                     autoRender: true,
@@ -166,10 +166,55 @@ Ext.define('Kmbsvle.view.apply.form.ApplyForm', {
                             click: 'openUploadForm'
                         }    
                     }
-                } 
+                }
+            ]
+        },
+        {
+            layout: {
+                type: 'hbox',
+                pack: 'start'                    
+            },
+            items: [
+                {
+                    xtype: 'fieldset',
+                    title: 'Персональні дані',
+                    width: 720,
+                    layout: 'anchor',
+                    defaults: {
+                        anchor: '100%'
+                    },
+                    items: [
+                        {
+                            xtype: 'fieldcontainer',
+                            layout: 'hbox',
+                            combineErrors: true,
+                            items: [
+                                {
+                                    xtype: 'datefield',
+                                    labelAlign: 'right',
+                                    name: 'birthDate',
+                                    fieldLabel: 'Народження',
+                                    emptyText: 'дата народження',
+                                    width: 300,
+                                    labelWidth: 100,
+                                    flex: 3
+                                },
+                                {
+                                    xtype: 'textfield',
+                                    name: 'birthPlace', 
+                                    fieldLabel: 'Місце народження',
+                                    hideLabel: 'true',
+                                    emptyText: 'місце народження',
+                                    reference: 'birthPlace',
+                                    margin: '0 0 0 50',
+                                    flex: 2                                    
+                                }
+                            ]
+                        }
+                    ]
+                }
             ]
         }
-               
     ],
     buttons: [
         { 
