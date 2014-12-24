8278451f35b107817d899c9316f84fb4c6340b8b
Vic on Mac <roxolanus@gmail.com>
2014-12-24 19:54:53 +0200

    seems that treestore leafs are not designed to have some wrapping of long titles, they get off track

--- src/main/webapp/app/store/CourseSelectionProfLeftMenuListStore.js
+++ src/main/webapp/app/store/CourseSelectionProfLeftMenuListStore.js
@@ -4,7 +4,11 @@ Ext.define('Kmbsvle.store.CourseSelectionProfLeftMenuListStore', {
         expanded: true,
         children: [
             {text: "Філософія менеджменту", leaf: true},
-            {text: "Корпоративна соціальна відповідальність та етика бізнесу", leaf: true}
+            {
+                text: '<div style="white-space:normal !important;">Корпоративна соціальна відповідальність та етика бізнесу</div>',
+                leaf: true
+                // renderer: function(value) { return '<div style="white-space:normal !important;">' + value + '</div>';}
+            }
         ]
     }
 });
